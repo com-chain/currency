39101659cd0f96c1d5a41f250ac73c5a65b5d3e9
Florian Dubath <florian.dubath@unige.ch>
2020-05-29 17:45:26 +0200

    Dispatching Smart Contract

--- dispatch.sol
+++ dispatch.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.11;
+pragma solidity ^0.4.18;
 
 /********************************************************
 
@@ -13,7 +13,7 @@ This contract recive eth and dispatch them to a list of contracts.
 contract owned {
   address public owner;
 
-  constructor() public{
+ function owned() public{
     owner = msg.sender;
   }
   
@@ -35,41 +35,48 @@ contract dispatch is owned {
 
   // list of targets
   address[] target_addresses;
+  uint max_add = 10;
+  
+  event UnableToDispatchTo(uint256 time, address target);
+  event Recieved(uint256 time, uint amount);
   
   
   /***** Contract administration *******/
   /* Transfert ownership */
   function transferOwnership(address newOwner) public onlyOwner {
-    accountType[newOwner] = 2;
-    accountStatus[newOwner] = true;
     owner = newOwner;
   }
   
   
   /*** Targets administrations ****/
    /* Count the number of Targets  */
-  function targetCount() public constant onlyOwner returns (uint256){
+  function targetCount() public constant returns (uint256){
     return target_addresses.length;
   }
   
   /* Get target at index  */
-  function getTarget(uint index) public constant onlyOwner returns (address target) {
+  function getTarget(uint index) public constant returns (address target) {
     return target_addresses[index];
   }
 
   
   /* Add new target ***/
   function addTarget(address newTarget) public onlyOwner {
+      
+    if (target_addresses.length>=max_add){
+        revert();
+    }
+    
     bool found = false;
     uint i;
     for (i = 0; i<target_addresses.length; i++){
-        if (!found && target_addresses[i] == target){
+        if (!found && target_addresses[i] == newTarget){
             found=true;
         }
     }
     
     if (!found){
-        target_addresses.push(target);
+        target_addresses.push(newTarget);
     }
   }
   
@@ -90,6 +97,7 @@ contract dispatch is owned {
 
     if (found){
         delete target_addresses[target_addresses.length-1]; // remove the last record from the array
+        target_addresses.length--;
     }
   }
   
@@ -97,44 +105,67 @@ contract dispatch is owned {
   /* In the case we need to retrieve Eth from the contract. Sent it back to the Owner */
   function repay(uint _amount) public onlyOwner {
       uint amount = _amount * 1 ether;
+      if(amount> this.balance){
+          amount= this.balance;
+      }
       owner.transfer(amount); 
   }
   
   /***** Ether handling *******/
   /* The contract dispatch Eth: it must be able to recieve them */
-  function () public payable{
-    //msg.value
+  function () public payable {
+    Recieved(now, msg.value/(1 ether));
   
-    uint[] balances;
-    uint max_balance=0;
-    uint i;
-    for (i = 0; i<target_addresses.length; i++){
-        uint curr_balance = target_addresses[i].balance;
-        balances.push(curr_balance);
+    uint add_number = target_addresses.length;
+    if (add_number>0) {
+    
+        uint256[] memory  balances = new uint256[](add_number);
+        uint256 max_balance=0;
+        uint i;
+        for (i = 0; i<add_number; i++){
+        uint256 curr_balance = target_addresses[i].balance;
+        balances[i]=curr_balance;
         if (max_balance<curr_balance) {
             max_balance=curr_balance;
         }
     }
     max_balance = max_balance+1;
-    uint total=0;
+    uint256 total=0;
     for (i = 0; i<target_addresses.length; i++){
-        balances[i] = max_balance - balances[i];
+        balances[i] = max_balance + 1 - balances[i];
         total = total + balances[i];
     }
     
-    uint base_amount = msg.value;
+    uint256 base_amount = msg.value;
     for (i = 0; i<target_addresses.length; i++){
-        target_addresses.send((base_amount*balances[i])/total)
+       if (! target_addresses[i].send((base_amount*balances[i])/total)){
+          UnableToDispatchTo(now, target_addresses[i]); //0.4.21 add emit
+       }
     }
     
-    base_amount = this.value;  // in solidity 0.5 => address(this)
-    for (i = 0; i<target_addresses.length; i++){
-        target_addresses.send((base_amount*balances[i])/total)
+    base_amount = this.balance;  // in solidity 0.5 => address(this)
+    if (base_amount> 1 ether){
+       for (i = 0; i<target_addresses.length; i++){
+          if (! target_addresses[i].send((base_amount*balances[i])/total)){
+             UnableToDispatchTo(now, target_addresses[i]); //0.4.21 add emit
+          }
+       }
+    }
     }
     
   }
-
+}
  
+
+
+
+------------------------
+dispatch contract
+
+
+
+[ { "constant": true, "inputs": [ { "name": "_user", "type": "address" } ], "name": "isOwner", "outputs": [ { "name": "", "type": "bool", "value": false } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [ { "name": "_amount", "type": "uint256" } ], "name": "repay", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [ { "name": "newTarget", "type": "address" } ], "name": "addTarget", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "owner", "outputs": [ { "name": "", "type": "address", "value": "0x4868db83bcf1a129eac577d4d1ad0fd5676176c6" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "targetCount", "outputs": [ { "name": "", "type": "uint256", "value": "0" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [ { "name": "target", "type": "address" } ], "name": "removeTarget", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [ { "name": "index", "type": "uint256" } ], "name": "getTarget", "outputs": [ { "name": "target", "type": "address", "value": "0x" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [ { "name": "newOwner", "type": "address" } ], "name": "transferOwnership", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "payable": true, "stateMutability": "payable", "type": "fallback" }, { "anonymous": false, "inputs": [ { "indexed": false, "name": "time", "type": "uint256" }, { "indexed": false, "name": "target", "type": "address" } ], "name": "UnableToDispatchTo", "type": "event" } ]
+
 
 
 
