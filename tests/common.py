324f6ee635a976a76eb9cca4cc9020791be770ef
Valentin Lab <valentin.lab@kalysto.org>
2023-01-09 11:47:25 +0100

    new: dev: introduce ``c`` fixture for direct currency access.
    ``c`` is meant to be used to access the public values and readable
    function of the contract. See examples.

--- tests/common.py
+++ tests/common.py
@@ -5,6 +5,7 @@ from pytest import fixture
 from brownie import ComChainCurrency, accounts, reverts
 from brownie.network.contract import ContractCall, ContractTx
 
+
 class Account(object):
 
     def __init__(self, currency, account):
@@ -15,12 +16,36 @@ class Account(object):
         if hasattr(self._currency, key):
             fn = getattr(self._currency, key)
             if isinstance(fn, ContractCall):
+                if len(fn.abi["inputs"]) == 0:
+                    return fn()
                 return lambda: fn(self._account)
             elif isinstance(fn, ContractTx):
                 return lambda *args: \
                     fn(*[a._account if isinstance(a, Account) else a
                          for a in args],
                        {"from": self._account})
+            else:
+                import pdb; pdb.set_trace()
+                pass
+        raise AttributeError
+
+
+class Currency(object):
+
+    def __init__(self, currency):
+        self._currency = currency
+
+    def __getattr__(self, key):
+        if hasattr(self._currency, key):
+            fn = getattr(self._currency, key)
+            if isinstance(fn, ContractCall):
+                if len(fn.abi["inputs"]) == 0:
+                    return fn()
+                return lambda *args: \
+                    fn(*[a._account if isinstance(a, Account) else a
+                         for a in args])
+            elif isinstance(fn, ContractTx):
+                raise Error("Must call from an account")
             else:
                 import pdb; pdb.set_trace()
                 pass
@@ -39,14 +64,21 @@ class MkAccounts(object):
             return Account(self._currency, self._accounts[key])
         raise KeyError(key)
 
+
 @fixture(autouse=True)
 def currency():
     return accounts[0].deploy(ComChainCurrency, "Broutzouf", "Br")
 
 
 @fixture
+def c(currency):
+    return Currency(currency)
+
+
+@fixture
 def Accounts(currency):
     return MkAccounts(currency, accounts)
+
 
 @fixture
 def owner(Accounts):
